9445045d2c2a0648eee5a4e1f7b784b853bb03e5
yao.peng <pengyao_2@163.com>
2016-08-15 16:04:46 +0800

    数组多选

--- js/controller/contactsDetailCtrl.js
+++ js/controller/contactsDetailCtrl.js
@@ -8,5 +8,19 @@ define(["../modules"],function (md) {
        $scope.edit = function (item) {
             item.edit = !item.edit;
        }
+	   $scope.typeData = [
+	        {name:"朋友",value:"1"},
+	        {name:"同事",value:"2"},
+	        {name:"同学",value:"3"},
+	        {name:"陌生人",value:"4"}
+	   ];
+	    $scope.typeChecked = function (item,val){
+            var idx = item.relation.indexOf(val);
+            if (idx > -1) {
+                item.relation.splice(idx, 1);
+            } else {
+                item.relation.push(val);
+            }
+        }
    }]);
 });
